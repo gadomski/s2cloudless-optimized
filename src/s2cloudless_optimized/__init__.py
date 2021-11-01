15e629c5ea48960db6fb303773f7c3206417c4c3
Pete Gadomski <pete.gadomski@gmail.com>
2021-11-01 15:08:39 -0600

    fix: compression

--- src/s2cloudless_optimized/__init__.py
+++ src/s2cloudless_optimized/__init__.py
@@ -1,13 +1,15 @@
 from typing import Optional, Union
 
+from .constants import DEFAULT_COMPRESSION, DEFAULT_RESOLUTION
 from .enums import HighLow
 from .granule import Granule, OutputPaths
 
 
 def run(
     directory: str,
-    directory_is_granule: Optional[bool] = True,
-    resolution: Optional[Union[float, HighLow]] = None,
+    directory_is_granule: bool = True,
+    compression: str = DEFAULT_COMPRESSION,
+    resolution: Union[float, HighLow] = DEFAULT_RESOLUTION,
     output_directory: Optional[str] = None,
 ) -> OutputPaths:
     """Runs s2cloudless-optimized on the Sentinel2 L1C files in the given directory.
@@ -20,4 +22,6 @@ def run(
         granule = Granule.from_granule_path(directory)
     else:
         granule = Granule(directory)
-    return granule.run(resolution, output_directory=output_directory)
+    return granule.run(
+        resolution, compression=compression, output_directory=output_directory
+    )
---
+++ src/s2cloudless_optimized/constants.py
@@ -0,0 +1,5 @@
+from .enums import HighLow
+
+BANDS = ["B01", "B02", "B04", "B05", "B08", "B8A", "B09", "B10", "B11", "B12"]
+DEFAULT_RESOLUTION = HighLow.HIGH
+DEFAULT_COMPRESSION = "lzw"
